b00738b8996b3d8b1e38f5a05e631075cad7be8e
rowdata <karanrathod7031@gmail.com>
2026-03-06 01:32:28 +0530

    Update API URL to production endpoint

--- frontend/src/services/assetService.js
+++ frontend/src/services/assetService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/assets';
+const API_URL = 'https://study-material-19vf.onrender.com';
 
 export const assetService = {
   fetchAssets: async (parentId) => {
